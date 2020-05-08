29af85116e3c47a9d9fe13f8ee3ef3843721be89
Rene Scott Simonsen <rss@Knowhowdata.dk>
2020-05-08 20:55:24 +0200

    Add project files.

---
+++ CollectionViewIssues/CollectionViewIssues/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CollectionViewIssues
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var data = new int[50];
+            for (var i = 0; i < data.Length; i++)
+            {
+                data[i] = i;
+            }
+
+            CollectionView.ItemsSource = data;
+        }
+    }
+}
